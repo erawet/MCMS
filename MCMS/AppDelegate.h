6b2bdeb7248d21bdb1cccff0bfb6143844031838
Don Wettasinghe <dwettasinghe@drfirst.com>
2015-01-18 13:03:05 -0500

    Initial Commit

---
+++ MCMS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MCMS
+//
+//  Created by Don Wettasinghe on 1/18/15.
+//  Copyright (c) 2015 com.don. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MCMS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MCMS
+//
+//  Created by Don Wettasinghe on 1/18/15.
+//  Copyright (c) 2015 com.don. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
